7732517913af5f307308172b69e25096057304fa
suedoh <suedoh36@proton.me>
2023-03-08 23:52:56 -0500

    added interface

--- status/status.go
+++ status/status.go
@@ -2,9 +2,15 @@ package status
 
 type Links []string
 
-func (l Links) Make(links []string)  {
+type StatusChecker interface {
+    Make([]string) []string
+}
+
+func (l Links) Make(links []string) []string {
     for _, link := range links {
         l = append(l, link)
     }
+
+    return l
 }
 
