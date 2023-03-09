b3a284f66af4ce61b255105fa2d251daa24e9d55
suedoh <suedoh36@proton.me>
2023-03-09 00:34:12 -0500

    started user input for web scraping

--- main.go
+++ main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
+    "bufio"
+    "fmt"
+    "io"
+    "net/http"
+    "os"
+
+    "github.com/suedoh/go-http-scraper/status"
 )
 
 type logWriter struct {}
 
 func main() {
+    links := []string{
+        "http://duckduckgo.com",
+        "http://cht.sh",
+        "http://github.com",
+        "http://hotmail.com",
+        "http://golang.com",
+    }
+
+    status := &status.Checker{}
+    linksToCheck := status.Make(links)
+    status.Check(linksToCheck)
+
+    for {
+        reader := bufio.NewReader(os.Stdin)
+        fmt.Println("Would you like to scrape HTML? (y/n)")
+        fmt.Print("Enter answer: ")
+        answer, _ := reader.ReadString('\n')
+
+        // change to switch
+        if answer == "n" || answer == "no" {
+            fmt.Println("k Bye")
+            os.Exit(1)
+        } else {
+            break
+        }
+
+    }
+
     if len(os.Args) != 2 {
         fmt.Println("Please include a web address to scrape as an argument")
         os.Exit(1)
